e2f61330cf46e344efb4d622311c32bb910a1433
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-06-14 07:51:14 +0100

    - ifx

--- src/main/java/com/java6/springtopic/processor/UserProcessor.java
+++ src/main/java/com/java6/springtopic/processor/UserProcessor.java
@@ -1,14 +1,18 @@
 package com.java6.springtopic.processor;
 
+import com.java6.springtopic.config.ComponentConfig;
 import com.java6.springtopic.service.AddressLocatorService;
 import com.java6.springtopic.service.PhotoRecognitionService;
 
 public class UserProcessor {
+    private ComponentConfig componentConfig1 = new ComponentConfig("K1","K2","K3");
+    private ComponentConfig componentConfig2 = new ComponentConfig("K11","K22","K33");
+
     private AddressLocatorService addressLocatorService
-            = new AddressLocatorService("asd","lva");
+            = new AddressLocatorService("asd","lva", componentConfig1);
     private PhotoRecognitionService photoRecognitionService
             = new PhotoRecognitionService(
-                    "Key1", 11, "decorator");
+                    "Key1", 11, "decorator", componentConfig2);
 
     public void doJob(){
         addressLocatorService.doJob();
