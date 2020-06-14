4f464f5ee698944f8c165bfe43eb90fcea76d7e9
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2020-06-14 09:29:42 +0100

    - get bean

--- src/main/java/com/java6/springtopic/Umain.java
+++ src/main/java/com/java6/springtopic/Umain.java
@@ -1,7 +1,24 @@
 package com.java6.springtopic;
 
+import com.java6.springtopic.processor.SecurityProcessor;
+import com.java6.springtopic.processor.UserProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Umain {
     public static void main(String args[]){
-        System.out.println("Hello java6");
+        UserProcessor userProcessor = new UserProcessor();
+        userProcessor.doJob();
+
+        SecurityProcessor securityProcessor = new SecurityProcessor();
+        securityProcessor.doJob();
+
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("beans.xml");
+
+        MyFirstSpringBean myFirstSpringBean
+                = (MyFirstSpringBean) context.getBean("myFirstSpringBean");
+        myFirstSpringBean.drukat();
+
     }
 }
